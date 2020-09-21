5a91294ee868b54dd80f03f80cbf87f930980eff
Nikita Zhuchkevich <n.zhuchkevich@itransition.com>
2020-09-21 18:51:20 +0300

    Set up environment

--- src/main/java/admin/admin/AdminApplication.java
+++ src/main/java/admin/admin/AdminApplication.java
@@ -2,8 +2,12 @@ package admin.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EntityScan(basePackages = "models.models")
 public class AdminApplication {
 
 	public static void main(String[] args) {
